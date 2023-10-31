16534b7bbfda86eb9cb24e0f274b27e88cb99749
DAVINA SINGH <davina_singh@outlook.com>
2023-10-31 14:18:36 +0000

    map

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from 'react';
+import { useState } from "react";
 import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
@@ -7,6 +7,8 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 function App() { 
   const [location, setLocation] = useState({});
   const [search, setSearch] = useState("");
+  const [number, setNumber] = useState(10);
+
 
   function handleChange(event) {
     setSearch(event.target.value);
@@ -23,18 +25,33 @@ function App() {
   setLocation(res.data[0]);
 }
 
+  function handleNumber(mod) {
+    setNumber(number + mod);
+  }
+
   return (
     <>
     <h1>APIs</h1>
     <form onSubmit={getLocation}>
-      <input onChange={(handleChange)} placeholder="Location" />
+      <input onChange={handleChange} placeholder="Location" />
       <button>Get Location</button>
     </form>
 
-    <h2>{location.lat}</h2>
-    <h2>{location.lon}</h2>
-      {/* a form with an input for the user to search */}
-      {/* information about the location saved in state */}
+    {location.lat && (
+      <div>
+        <button onClick={() => handleNumber(-1)}>-</button>
+        <span>{number}</span>
+        <button onClick={() => handleNumber(1)}>+</button>
+        
+        <img
+            src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${location.lat},${location.lon}&zoom=${number}&format=png`}
+          />
+      </div>
+    )}
+     
+    <h2>{location.display_name}</h2>
+    <p>{location.lat}</p>
+    <p>{location.lon}</p>
     </>
   );
   }
